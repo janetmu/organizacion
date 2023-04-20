4879110f7d08bda8c6ade9d2bcf04b8b4c00bd57
Janet Marulanda <janet_marulanda@yahoo.com>
2023-04-19 20:33:20 -0500

    Creacion formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import Header from './components/Header/Header';
+import './App.css'
+import Header from './components/Header/Header'
+import Formulario from './components/Formulario/Formulario'
 
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Formulario />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import "./Header.css"
 
 function Header () {
-    return <header>
-        <img src="img/header.png" alt="Org" />;
+    return <header className="header">
+        <img src="img/header.png" alt="Org" />
     </header>
 }
 
