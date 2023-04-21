246ed696f3126dc7f97583db049ab99db1dde368
Janet Marulanda <janet_marulanda@yahoo.com>
2023-04-20 22:03:01 -0500

    terminación formulario con creacion lista de opciones y boton de envio

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,14 +1,24 @@
 import "./Formulario.css"
-import CampoTexto from "../CampoTexto/CampoTexto"
+import CampoTexto from "../CampoTexto"
+import ListaOpciones from "../ListaOpciones"
+import Boton from "../Boton"
 
 const Formulario = () => {
+
+  const manejarEnvio = (event) => {
+    event.preventDefault()
+    console.log (manejarEnvio, event)
+  }
+
     return (
       <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
           <h2>Rellena el formulario para crear el colaborador.</h2>
           <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" />
           <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
           <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
+          <ListaOpciones />
+          <Boton>Crear</Boton> 
         </form>
       </section>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
-import "./Header.css"
+import "./Header.css";
 
-function Header () {
-    return <header className="header">
-        <img src="img/header.png" alt="Org" />
+function Header() {
+  return (
+    <header className="header">
+      <img src="img/header.png" alt="Org" />
     </header>
+  );
 }
 
-export default Header
+export default Header;
